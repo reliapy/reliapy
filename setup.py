ac033cd5486f746af9abc60411560418cd927af9
ketsonroberto <ketson.santos@gmail.com>
2021-09-05 17:38:20 +0200

    setup update

--- setup.py
+++ setup.py
@@ -3,25 +3,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DICpy',
-    version="0.3.1",
-    url='https://github.com/eesd-epfl/DICpy',
-    description="Digital Image Correlation with Python.",
+    name='reliapy',
+    version="1.0.0",
+    url='https://github.com/reliapy/reliapy',
+    description="Structural Risk and Reliability with Python.",
     author="Ketson R. M. dos Santos",
     author_email="ketson.santos@epfl.ch",
-    license='MIT',
+    license='BSD',
     platforms=["OSX", "Windows", "Linux"],
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"": ["*.pdf"]},
     install_requires=[
-        "numpy", "scipy", "matplotlib", "scikit-learn", "scikit-image", "opencv-python"
+        "numpy", "scipy", "matplotlib", "scikit-learn"
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Image Processing',
-        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
     ],
 )
